53381f3bc98da3c9bd6aeb172efb6d3d33b96e99
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2021-02-04 15:09:42 -0300

    Creada UI vista register

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -15,6 +15,45 @@
                 <umt-date-picker label="fecha de nacimiento" placeholder="Ingresa tu fecha de nacimiento" :max="getMaxDate" :min="getminDate" />
             </a-col>
         </a-row>
+        <a-row type="flex" justify="center">
+            <a-col :span="18">
+                <umt-radio-group v-model="gender" name="gender" label="sexo">
+                    <a-row type="flex" :gutter="12">
+                        <a-col :span="12">
+                            <umt-radio value="M">
+                                Masculino
+                            </umt-radio>
+                        </a-col>
+                        <a-col :span="12">
+                            <umt-radio value="F">
+                                Femenino
+                            </umt-radio>
+                        </a-col>
+                    </a-row>
+                </umt-radio-group>
+            </a-col>
+        </a-row>
+
+        <a-row type="flex" justify="center">
+            <a-col :span="18">
+                <p class="info">
+                    *Tu edad y sexo permitirán a otros rivales
+                    encontrarte y desafiarte en un match.
+                </p>
+            </a-col>
+        </a-row>
+
+        <a-row type="flex" justify="center">
+            <a-col :span="18">
+                <umt-button>registrar</umt-button>
+            </a-col>
+        </a-row>
+
+        <a-row class="terms" type="flex" justify="center">
+            <a-col>
+                <a>Términos y condiciones</a>
+            </a-col>
+        </a-row>
     </div>
 </template>
 
@@ -28,7 +67,8 @@
         data () {
             return {
                 stepTitle: 'ingresa tus datos',
-                stepBackView: 'start'
+                stepBackView: 'start',
+                gender: 'M'
             }
         },
         computed: {
